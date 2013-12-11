feeb2b4661d1424af957fcbe260f3e2347aea9d3
Deniz Veli <dveli@etsy.com>
2013-12-11 16:43:49 -0500

    Guard against wild view types

--- library/src/main/java/com/etsy/android/grid/StaggeredGridView.java
+++ library/src/main/java/com/etsy/android/grid/StaggeredGridView.java
@@ -777,7 +777,8 @@ public class StaggeredGridView extends ExtendableListView {
             final View child = getChildAt(i);
             if (child != null) {
                 final LayoutParams childParams = (LayoutParams) child.getLayoutParams();
-                if (childParams.viewType != ITEM_VIEW_TYPE_HEADER_OR_FOOTER) {
+                if (childParams.viewType != ITEM_VIEW_TYPE_HEADER_OR_FOOTER &&
+                        childParams instanceof GridLayoutParams) {
                     GridLayoutParams layoutParams = (GridLayoutParams) childParams;
                     int column = layoutParams.column;
                     int position = layoutParams.position;
