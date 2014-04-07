ba667410fa7b2a0700c76cc506e8f36fe5256d62
Dave Cole <davejohncole@gmail.com>
2014-04-07 20:21:17 +1000

    Fix the adapter index to getItemId() to include the mFirstPosition offset.

--- library/src/main/java/com/etsy/android/grid/ExtendableListView.java
+++ library/src/main/java/com/etsy/android/grid/ExtendableListView.java
@@ -2741,7 +2741,7 @@ public abstract class ExtendableListView extends AbsListView {
                 final View view = getChildAt(motionPosition); // a fix by @pboos
 
                 if (view != null) {
-                    performItemClick(view, motionPosition + mFirstPosition, adapter.getItemId(motionPosition));
+                    performItemClick(view, motionPosition + mFirstPosition, adapter.getItemId(motionPosition + mFirstPosition));
                 }
             }
         }
